d6797ee32c02ad166dfeb7c8a4bc49260479997f
GraymonDGT <38462718+GraymonDgt@users.noreply.github.com>
2026-01-05 11:56:26 -0500

    Update Options.py

--- pvzf/Options.py
+++ pvzf/Options.py
@@ -7,16 +7,31 @@ from Options import DefaultOnToggle, Range, Toggle, DeathLink, Choice, PerGameCo
 class CompletionType(Choice):
     """Set goal for victory
     Zomboss - complete the Dr. Zomboss' Revenge mini-game (unlocks in the minigames tab with roof)
-    All X-9s - Complete every main area's 9th level"""
+    All X-9s - Complete every main area's 9th level
+    Odyssey Adventure - Complete Odyssey Adventure: Level 15
+    Trophy Hunt - Collect a percentage of trophies"""
     option_zomboss = 0
     option_all_x9s = 1
+    option_odyssey_adventure = 2
+    option_trophy_hunt = 3
     default = 0
     #Odyssey Adventure - Complete Odyssey Adventure (not implemented)
     #Level Percentage - complete the specified percentage of levels (not implemented)
+    #Trophy Collectathon - find the specified number of trophies
 
 # when minigame-sanity is off
-
-
+class HuntNumber(Range):
+    """Number of trophies to be included for trophy hunt"""
+    display_name = "Trophy number"
+    range_start = 1
+    range_end = 100
+    default = 10
+class HuntPercentage(Range):
+    """PERCENTAGE of trophies required to goal for trophy hunt"""
+    display_name = "Trophy percentage"
+    range_start = 1
+    range_end = 100
+    default = 50
 #class GoalPercentage(Range):
 #    """PERCENTAGE of levels needed to goal with the Level percentage goal option
 #    THIS IS A PERCENTAGE OUT OF 100, NOT THE NUMBER OF LEVELS"""
@@ -37,24 +52,46 @@ class FusionChallengeSanity(Toggle):
     """Adds the 12 fusion challenge levels as an item/locations"""
     display_name = "Enable Fusion Challenges"
 
-class FusionShowcaseSanity(Toggle):
-    """Adds the 88 fusion showcase levels as an item/locations
-    CURRENTLY NON-FUNCTIONAL"""
-    display_name = "Enable Fusion Showcase"
+#class FusionShowcaseSanity(Toggle):
+#   """Adds the 88 fusion showcase levels as an item/locations
+#    CURRENTLY NON-FUNCTIONAL"""
+#    display_name = "Enable Fusion Showcase"
+class TenFlagSanity(Toggle):
+    """Adds the 12 Ten-flag levels as items/locations"""
+    display_name = "Enable Ten-flag challenges"
 
 class MinigameSanity(Choice):
-    """Short: Adds 21 shorter 2-flag Minigames as items/locations
+    """Short: Adds 22 shorter 2-flag Minigames as items/locations
     All: Adds all 50 normal mini-games as items/locations"""
     option_off = 0
     option_short = 1
     option_all = 2
     default = 0
 
+class VasebreakerSanity(Toggle):
+    """Adds the 3 vasebreaker levels as an item/locations"""
+    display_name = "Enable Vasebreaker"
+
+
+class SurvivalSanity(Toggle):
+    """Adds the 10 survival mode levels as items/locations"""
+    display_name = "Enable Survival Mode"
+
+
+
+class AdventureOdyssey(Toggle):
+    """Adds the 15 Odyssey Adventure levels into the pool as items/locations"""
+    display_name = "Enable Adventure Odyssey"
+
     #display_name = "Enable Mini-games"
 
 class SeedSlots(DefaultOnToggle):
     """Randomize seed slots"""
     display_name = "Seed Slots"
+
+class LawnCleaners(Toggle):
+    """Randomize lawnmowers/pool cleaners"""
+    display_name = "Lawnmowers"
 
 class TrapPercentage(Range):
     """Percentage of filler items to replace with traps"""
@@ -101,15 +138,19 @@ class UniquePlantBlacklist(OptionList):
     """Blacklist for unique plants, keys are by almanac entry
      Set Unqiue plants to 'on' and blacklist specific ones here"""
     display_name = "Unique Plant Blacklist"
-    valid_keys = {"Cattail Girl","Swordmaster Starfruit","Nyan Squash","Burger Blaster","Queen Endoflame","Coldsnap Bean","Amp-nion","Sniper Pea","Chrysanctum","Icetip Lily","Pearmafrost","Doubleblast Passionfruit"}
+    valid_keys = {"Cattail Girl","Swordmaster Starfruit","Nyan Squash","Burger Blaster","Queen Endoflame","Coldsnap Bean","Amp-nion","Snipea","Chrysanctum","Icetip Lily","Pearmafrost","Doubleblast Passionfruit","Lucky Blover","Diamond Imitater"}
 
 
 pvzf_options_groups = [
 OptionGroup("Location Toggles", [
         AdventureExtra,
-        FusionShowcaseSanity,
+        #FusionShowcaseSanity,
         FusionChallengeSanity,
-        MinigameSanity
+        #TenFlagSanity,
+        MinigameSanity,
+        VasebreakerSanity,
+        SurvivalSanity,
+        AdventureOdyssey
 #        StartingCharacter,
 #        OneUpSanity,
 #        SuperRingSanity,
@@ -117,7 +158,10 @@ OptionGroup("Location Toggles", [
  ]),
 OptionGroup("Meta Options", [
     CompletionType,
+    HuntNumber,
+    HuntPercentage,
     SeedSlots,
+    LawnCleaners,
     TrapPercentage,
     LogicDifficulty,
     RingLink,
@@ -131,16 +175,23 @@ OptionGroup("Meta Options", [
 @dataclass
 class PVZFOptions(PerGameCommonOptions):
     goal_type: CompletionType
+    trophy_number:HuntNumber
+    trophy_percentage:HuntPercentage
     adventure_extra: AdventureExtra
-    showcase_sanity: FusionShowcaseSanity
+    #showcase_sanity: FusionShowcaseSanity
     challenge_sanity: FusionChallengeSanity
+    #ten_flag_sanity:TenFlagSanity
     minigame_sanity: MinigameSanity
+    vasebreaker_sanity:VasebreakerSanity
+    survival_sanity:SurvivalSanity
+    adventure_odyssey: AdventureOdyssey
     randomize_seed_slots: SeedSlots
+    randomizer_mowers: LawnCleaners
     trap_percentage: TrapPercentage
     logic_difficulty: LogicDifficulty
     ring_link: RingLink
     death_link: DeathLink
     unique_preset: UniquePlantsPreset
-    no_start_plant: StartingPlantBlacklist
     unique_blacklist: UniquePlantBlacklist
+    no_start_plant: StartingPlantBlacklist
 
